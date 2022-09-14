e84476d0f5e3f0d78cf8cd5275018c5ecf03b51d
Jesus Borjas <jesusbor006@gmail.com>
2022-09-13 22:27:10 -0500

    Statements & Expression file added

---
+++ statements&Expressions.js
@@ -0,0 +1,18 @@
+// Statement
+// A single piece of code that accomplishes one task or action
+
+// Expression: A statement that produces a value
+
+// Operator: Symbols that are used to assign, compare, and perform operations
+
+console.log(12 / 3);
+console.log(12 % 5);
+console.log('hello' + ' world' + '!');
+console.log('4' === 4);
+
+var age = 22;
+if (age > 21 && age < 65) {
+  console.log('Between 21-65');
+} else {
+  console.log('Very young or very old');
+}
